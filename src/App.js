b36d47c765876348af112193dabfa058e2c42e3a
Santje777 <s.v.oosterom@gmail.com>
2024-06-18 21:11:44 +0200

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-
+import Dictionary from "./Dictionary";
+import Footer from "./Footer";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-    
+        <h1>Welcome to Susannes dictionary app</h1>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">
+        <Footer />
+      </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/Footer.js
@@ -0,0 +1,35 @@
+import "./Footer.css";
+
+export default function Footer() {
+    return (
+      <div className="footer">
+        This weather app project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/susanne-van-oosterom-96a6991b2/"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="link to Susanne's LinkedIn page"
+        >
+          Susanne van Oosterom
+        </a>{" "}
+        and is open sourced on{" "}
+        <a
+          href="https://github.com/Santje777/dictionary-project"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="link to the github page of this site"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://susannesdictionary.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="link to netlify page"
+        >
+          Netlify
+        </a>
+      </div>
+    );
+  }
